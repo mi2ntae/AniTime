90207c833b168c6d349a11f6946230f9ee32c61c
김민태 <kimmintae981019@gmail.com>
2023-07-24 17:51:19 +0900

    Feat: AuthenticationEnrtyPoint 처리
    Former-commit-id: df91a69976303f14b791d5299c9a4fd16487d584

--- backend/src/main/java/com/moi/anitime/exception/ExceptionList.java
+++ backend/src/main/java/com/moi/anitime/exception/ExceptionList.java
@@ -13,7 +13,8 @@ public enum ExceptionList {
     AUTHENTICATION_ENTRYPOINT(-1002, "해당 기능을 이용하기 위한 권한이 없습니다."),
     DUPLICATE_BOOKMARK(-1009, "이미 찜이 되어있는 유기 동물입니다."),
     OPEN_BANK_TRANSFER_ERROR(-1022, "결제에 실패하였습니다."),
-    NON_VALID_JWT_TOKEN(-1023, "올바르지 않은 토큰입니다. 다시 로그인하세요");
+    AUTHENTICATION_ENTRY_POINT(-1023, "토큰을 확인할 수 없습니다. 다시 로그인하세요"),
+    NON_VALID_JWT_TOKEN(-1024, "올바르지 않은 토큰입니다. 다시 로그인하세요");
 
     private final int code;
     private final String message;
