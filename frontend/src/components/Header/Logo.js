3936d222d4bd43e0ddd8461961d522e075bb2570
임성원 <dnjsndnjs@gmail.com>
2023-07-24 16:38:10 +0900

    Fix: Header 스타일 오류
    Former-commit-id: 598d615c8ccb2c5e22db3b1ca335b518f92cd8b9

--- frontend/src/components/Header/Logo.js
+++ frontend/src/components/Header/Logo.js
@@ -2,17 +2,6 @@ import React from "react";
 import { styled } from "styled-components";
 
 export default function Logo() {
-  const Div = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 160px;
-  `;
-  const Title = styled.p`
-    margin-left: 8px;
-    font-weight: bold;
-    font-size: 1.2rem;
-  `;
   return (
     <Div>
       <img src="/icons/logo.svg" alt="애니타임" />
@@ -20,3 +9,16 @@ export default function Logo() {
     </Div>
   );
 }
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 160px;
+`;
+
+const Title = styled.p`
+  margin-left: 8px;
+  font-weight: bold;
+  font-size: 1.2rem;
+`;
--- frontend/src/components/Header/NavBar.js
+++ frontend/src/components/Header/NavBar.js
@@ -2,24 +2,24 @@ import React from "react";
 import { styled } from "styled-components";
 
 export default function NavBar() {
-  const Link = styled.p`
-    color: #35383b;
-  `;
-
-  const NavBar = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    margin: auto;
-    width: auto;
-    max-width: 560px;
-  `;
-
   return (
-    <NavBar>
+    <Nav>
       <Link>입양하기</Link>
       <Link>실종동물</Link>
       <Link>후원하기</Link>
-    </NavBar>
+    </Nav>
   );
 }
+
+const Link = styled.p`
+  color: #35383b;
+`;
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: auto;
+  width: auto;
+  max-width: 560px;
+`;
