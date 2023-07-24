993ea5784f67e4bcfea61e4e491fd261012284c6
임성원 <dnjsndnjs@gmail.com>
2023-07-24 16:38:10 +0900

    Fix: Header 스타일 오류
    Former-commit-id: 598d615c8ccb2c5e22db3b1ca335b518f92cd8b9

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,40 +4,36 @@ import { styled } from "styled-components";
 import Logo from "./Logo";
 
 export default function Header() {
-  const Header = styled.header`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: space-around;
-    align-items: center;
-    height: 64px;
-    min-width: 480px;
-    max-width: 1240px;
-    margin: auto;
-  `;
-
-  const Grow = styled.div`
-    flex-grow: 1;
-  `;
-
-  const Right = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    width: 160px;
-  `;
-
   return (
-    <Header>
+    <StyleHeader>
       <Logo />
-      <Grow>
+      <div style={{ flexGrow: 1 }}>
         <NavBar />
-      </Grow>
+      </div>
       <Right>
         <img src="/icons/ic_help.svg" alt="help" />
         <img src="/icons/ic_notification.svg" alt="notification" />
         <img src="/icons/ic_account.svg" alt="account" />
       </Right>
-    </Header>
+    </StyleHeader>
   );
 }
+
+const StyleHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+  height: 64px;
+  min-width: 480px;
+  max-width: 1240px;
+  margin: auto;
+`;
+
+const Right = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 160px;
+`;
