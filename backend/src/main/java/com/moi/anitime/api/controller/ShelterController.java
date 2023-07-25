d9ef89c7ad6cc156163bc9beadc85f6e2c7aac91
권지훈 <aspire51@naver.com>
2023-07-25 15:20:20 +0900

    fix : branch merge bug fixed
    Former-commit-id: 060a1ff2153e818ef881df056138112ce416c178

--- backend/src/main/java/com/moi/anitime/api/controller/ShelterController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/ShelterController.java
@@ -1,9 +1,7 @@
-package com.moi.anitime.api.controller;
 
 import com.moi.anitime.api.response.SingleResponse;
 import com.moi.anitime.api.service.ResponseService;
 import com.moi.anitime.exception.member.NoExistMemberNoException;
-import com.moi.anitime.model.entity.member.GeneralMember;
 import com.moi.anitime.model.entity.member.Member;
 import com.moi.anitime.model.service.member.MemberService;
 import io.swagger.annotations.Api;
@@ -11,30 +9,27 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "멤버 API", tags = {"Member"})
+@Api(value = "멤버 API", tags = {"Shelter"})
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
-public class MemberController {
+@RequestMapping("/api/shelter")
+public class ShelterController {
     private final MemberService memberService;
     private final ResponseService responseService;
 
     @GetMapping("/{memberNo}")
-    @ApiOperation(value = "일반 회원 찾기", notes = "연습용.")
+    @ApiOperation(value = "보호소 회원 찾기", notes = "연습용.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = -1019, message = "없는 회원"),
+            @ApiResponse(code = -1025, message = "없는 회원"),
     })
     public SingleResponse findById(@PathVariable("memberNo") int memberNo) throws NoExistMemberNoException {
-        Member member = memberService.findGeneralMemberById(memberNo);
+        Member member = memberService.findShelterMemberById(memberNo);
         return responseService.getSingleResponse(member);
-    }
-
-    @PutMapping("/{memberNo}")
-    @ApiOperation(value="회원 정보 수정",notes="연습용")
-    public void editGeneralMember(@PathVariable("memberNo") int memberNo,@RequestBody GeneralMember requestMember){
-        memberService.editGeneralMember(memberNo,requestMember);
     }
 }
