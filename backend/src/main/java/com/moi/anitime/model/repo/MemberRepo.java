721527390697f9a3095b1135ea49318b3f3fbed1
DESKTOP-1JVI7QI\SSAFY <kimmintae981019@gmail.com>
2023-07-25 14:47:48 +0900

    Feat: MultipartFile 형식 정의 및 변환, 일반 회원 조회,
    Former-commit-id: e40d75cb0a2300eef6fe16020c381cbfd8bdbdf8

--- backend/src/main/java/com/moi/anitime/model/repo/MemberRepo.java
+++ backend/src/main/java/com/moi/anitime/model/repo/MemberRepo.java
@@ -2,6 +2,7 @@ package com.moi.anitime.model.repo;
 
 import com.moi.anitime.model.entity.member.GeneralMember;
 import com.moi.anitime.model.entity.member.Member;
+import com.moi.anitime.model.entity.member.ShelterMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,8 +15,8 @@ import java.util.Optional;
 @Repository
 public interface MemberRepo extends JpaRepository<Member, Integer> {
     public Optional<Member> findByEmail (@Param("email") String email);
-
-    @Transactional
+    public Optional<GeneralMember> findGeneralMemberByMemberNo(@Param("memberno") int memberNo);
+    public Optional<ShelterMember> findShelterMemberByMemberNo(@Param("memberno") int memberNo);
     @Modifying
     @Query("UPDATE Member m SET m.password = :password,m.name = :name WHERE m.memberNo=:memberNo")
     public void updateMemberByMemberNo(@Param("memberNo") int memberNo,@Param("password") String password,@Param("name") String name);
