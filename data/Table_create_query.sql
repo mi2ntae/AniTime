0677e3fd658af8de3238d544f799813c31696c7d
권지훈 <aspire51@naver.com>
2023-07-25 14:41:41 +0900

    fix : env setup
    Former-commit-id: b5f003c4aa8d99c9418132409379500d9b78a556

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -147,7 +147,7 @@ CREATE TABLE `Profile` (
 	`dateAt`	date	NULL,
 	`profileLocation`	varchar(255) NULL,
 	`lat`	float not	NULL,
-	`lon`	float not	NULL,
+	`lon`	float not	NULL,s09p12a208
 	`image`	varchar(255)	NULL
 )
 COMMENT='보호소 회원, 비회원의 공통된 회원 정보 저장을 위한 테이블'
