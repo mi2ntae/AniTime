c93b50dccc1519c25c101abcdff36b1766b083a6
김민태 <kimmintae981019@gmail.com>
2023-07-26 16:25:03 +0900

    Feat: Stomp 소켓 설정, Chat 관련 Controller, Service, Repo 생성, 채팅방 목록 조회 API 완성
    Former-commit-id: a16c22cf549d071fb7c38cd5d75c3aeee3f54ba1

--- backend/src/main/java/com/moi/anitime/api/controller/MessageController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.moi.anitime.api.controller;
 
+import com.moi.anitime.model.entity.chat.ChatMessage;
+import com.moi.anitime.model.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -10,13 +12,12 @@ import java.sql.SQLException;
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
-//    private final SimpMessagingTemplate smso;
-//    private final ChatService service;
-//
-//    @MessageMapping("/message")
-//    public void message(ChatMessage message) throws SQLException {
+    private final SimpMessagingTemplate smso;
+    private final ChatService service;
+
+    @MessageMapping("/message")
+    public void message(ChatMessage message) throws SQLException {
 //        service.addChat(message);
 //        smso.convertAndSend("/sub/"+message.getRoomId(), message);
-//    }
-
+    }
 }
