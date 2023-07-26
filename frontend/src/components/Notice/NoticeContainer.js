5e15f7bd45454a6c7faf09007acf8c75075090a2
임성원 <dnjsndnjs@gmail.com>
2023-07-26 12:33:28 +0900

    Feat: Notice Header Footer 스타일 적용
    Former-commit-id: 825dd818fa11885ad2ca85b332c5fe284534995a

--- frontend/src/components/Notice/NoticeContainer.js
+++ frontend/src/components/Notice/NoticeContainer.js
@@ -11,13 +11,16 @@ export default function NoticeContainer() {
         <HeaderItem>후원</HeaderItem>
         <HeaderItem>채팅</HeaderItem>
       </Header>
-      <div>
+      <Body>
         <p>알림 1 -- -- -- --</p>
         <p>알림 2 -- -- -- --</p>
         <p>알림 3 -- -- -- --</p>
         <p>알림 4 -- -- -- --</p>
-      </div>
-      <div>알림함 비우기</div>
+        <p>알림 4 -- -- -- --</p>
+        <p>알림 4 -- -- -- --</p>
+        <p>알림 4 -- -- -- --</p>
+      </Body>
+      <Footer>알림함 비우기</Footer>
     </Container>
   );
 }
@@ -33,10 +36,28 @@ const Header = styled.div`
   justify-content: flex-start;
   align-items: center;
   height: 50px;
+  box-sizing: border-box;
+  border-bottom: grey 1px solid;
 `;
 
 const HeaderItem = styled.a`
   margin: 0;
-  margin-left: 16px;
-  padding: 8px;
+  margin-left: 24px;
+`;
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  overflow: auto;
+  height: calc(100% - 100px);
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  box-sizing: border-box;
+  border-top: grey 1px solid;
 `;
