226030ab49d7c7534dc52ca25bae3b4cf9f8eaeb
김민태 <kimmintae981019@gmail.com>
2023-07-26 16:25:03 +0900

    Feat: Stomp 소켓 설정, Chat 관련 Controller, Service, Repo 생성, 채팅방 목록 조회 API 완성
    Former-commit-id: a16c22cf549d071fb7c38cd5d75c3aeee3f54ba1

--- backend/src/main/java/com/moi/anitime/model/entity/chat/ChatRoom.java
+++ backend/src/main/java/com/moi/anitime/model/entity/chat/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.moi.anitime.model.entity.chat;
 
+import com.moi.anitime.api.response.ChatRoomResponse;
 import com.moi.anitime.model.entity.member.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,34 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@NamedNativeQueries({
+        @NamedNativeQuery(
+                name = "findChatRoomsByGeneralNo",
+                query = "SELECT " +
+                        "chatroom.roomNo AS roomno, member.name AS shelterName, (SELECT content from chatMessage WHERE chatmessage.roomNo = chatroom.roomNo ORDER BY chatmessage.writtentime DESC LIMIT 1) AS lastmsg, " +
+                        "(SELECT COUNT(*) FROM chatmessage WHERE chatmessage.roomNo = chatroom.roomNo AND chatMessage.sendNo != 4 AND chatmessage.read = 0) AS unreadcnt " +
+                        "from chatroom " +
+                        "join Member on ChatRoom.shelterNo = Member.memberNo " +
+                        "join ChatMessage on ChatRoom.roomNo = ChatMessage.roomNo " +
+                        "where " +
+                        "ChatRoom.generalNo = :generalno " +
+                        "GROUP BY chatroom.roomNo;",
+                resultSetMapping = "findChatRoomsByGeneralNo"
+        )
+})
+@SqlResultSetMapping(
+        name = "findChatRoomsByGeneralNo",
+        classes = @ConstructorResult(
+                targetClass = ChatRoomResponse.class,
+                columns = {
+                        @ColumnResult(name = "roomNo", type = Integer.class),
+                        @ColumnResult(name = "shelterName", type = String.class),
+                        @ColumnResult(name = "lastMsg", type = String.class),
+                        @ColumnResult(name = "unreadCnt", type = Integer.class)
+
+                }
+        )
+)
 public class ChatRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
