e1cbb6939380a63fd19086247ce4fb08241af3a5
임성원 <dnjsndnjs@gmail.com>
2023-07-27 09:57:34 +0900

    Feat: NavBar 라우터 링크 적용, 커서 변경
    Former-commit-id: 0a51914f5a8ab01d39336dce934206bcb161a66f

--- frontend/src/components/Header/NavBar.js
+++ frontend/src/components/Header/NavBar.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 export default function NavBar() {
   return (
     <Nav>
-      <Link>입양하기</Link>
-      <Link>실종동물</Link>
-      <Link>후원하기</Link>
+      <LinkP to="desertion">입양하기</LinkP>
+      <LinkP to="missing">실종동물</LinkP>
+      <LinkP to="">후원하기</LinkP>
     </Nav>
   );
 }
 
-const Link = styled.p`
+const LinkP = styled(Link)`
   color: #35383b;
+  text-decoration: none;
 `;
 
 const Nav = styled.div`
