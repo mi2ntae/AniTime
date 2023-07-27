8a880993e467ec1098c20bbb7c5befdd047078dc
jaewoong <jwhur7@naver.com>
2023-07-27 10:44:22 +0900

    Rename: pages 폴더 생성/ 파일명 수정 및 이동
    Former-commit-id: f7a3a1089ae5299d59ad3d7911c935b8c10efc6d

---
+++ frontend/src/components/Desertion/Filter.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import Modal from "components/Modal/Modal";
+import {styled} from "styled-components";
+import FilterItem from "./FilterItem";
+
+export default function Filter() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = () => setIsOpen((p) => !p);
+    
+    const closeNotice = () => {
+        setIsOpen(false);
+    }
+    
+    return (
+        <>
+        <FilterButton
+         onClick={(event) => {
+            event.stopPropagation();
+            toggleOpen();
+        }}
+        >필터
+        {isOpen && (
+            <Modal posX="-75px" posY="-85px" close={closeNotice}>
+                <FilterItem/>
+            </Modal>
+        )}
+         </FilterButton>
+        </>
+    );
+
+}
+
+const FilterButton = styled.button`
+  margin-right:20px;
+  padding: 8px 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
