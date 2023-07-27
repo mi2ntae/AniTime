40d68c5f03dbd0b40be0fc6875cf819c20a578c0
임성원 <dnjsndnjs@gmail.com>
2023-07-27 09:40:47 +0900

    Rename: pages분리
    Former-commit-id: d9543bfba4043c42bde561c63349833b0f1a1941

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Main from "./components/Main/Main";
+import MainPage from "./pages/MainPage";
 import Desertion from "./components/Desertion/Desertion";
 import Missing from "./components/Missing/Missing";
-
 
 const router = createBrowserRouter([
   {
@@ -12,7 +11,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: <Main />,
+        element: <MainPage />,
       },
       {
         path: "desertion",
