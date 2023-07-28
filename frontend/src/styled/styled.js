f9934c940029af83dba3a41cdc726398a0725ff0
jaewoong <jwhur7@naver.com>
2023-07-28 15:44:44 +0900

    Refactor: 마이페이지 및 실종 페이지
    Former-commit-id: e2ecc90814579bbe77b6120f29d97cc3193cb9b4

--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,15 +1,21 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
+  ${({ $vertical }) =>
+    $vertical &&
+    css`
+      flex-direction: column;
+      justify-content: flex-start;
+    `}
   margin: auto;
   margin-top: 64px;
   padding: 20px;
   box-sizing: border-box;
   width: 100%;
   max-width: 1240px;
-  min-width: 480px;
+  min-width: 800px;
   min-height: calc(100vh - 160px);
 `;
 
