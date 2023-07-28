380c64d77d84fe18d4b7f56ed76af53268d84431
jaewoong <jwhur7@naver.com>
2023-07-28 15:44:44 +0900

    Refactor: 마이페이지 및 실종 페이지
    Former-commit-id: e2ecc90814579bbe77b6120f29d97cc3193cb9b4

--- frontend/src/pages/MissingPage.js
+++ frontend/src/pages/MissingPage.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { styled } from "styled-components";
-import { MainContainer} from "styled/styled";
+import { MainContainer } from "styled/styled";
 
 export default function Missing() {
   return (
     <PageContainer>
-        <MainContainer>
-          <ListFilterContainer>
-           <ListContainer>
-           <h3>실종 동물 목록</h3>
-           </ListContainer>
-           </ListFilterContainer>
-           <DetailViewBox>
+      <MainContainer>
+        <ListFilterContainer>
+          <ListContainer>
+            <h3>실종 동물 목록</h3>
+          </ListContainer>
+        </ListFilterContainer>
+        <DetailViewBox>
           <h3>상세정보창</h3>
-           </DetailViewBox>
+        </DetailViewBox>
       </MainContainer>
     </PageContainer>
   );
@@ -21,7 +21,7 @@ export default function Missing() {
 
 const PageContainer = styled.div`
   margin: auto;
-  display:flex;
+  display: flex;
   align-items: center;
   justify-content: center;
 `;
@@ -35,13 +35,13 @@ const ListFilterContainer = styled.div`
 `;
 
 const ListContainer = styled.div`
-  flex:1;
+  flex: 1;
   width: 700px;
   height: 600px;
   padding: 10px;
   overflow-y: scroll;
   text-align: center;
-  margin-top:73px;
+  margin-top: 74px;
   border: 1px solid #ccc;
   border-radius: 8px;
 `;
