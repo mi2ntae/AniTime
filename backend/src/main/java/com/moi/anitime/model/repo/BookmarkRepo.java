df0b8f9568629f20c57d968b4faf89cf88723bf6
lojo0307 <lojo0307@naver.com>
2023-07-28 13:44:35 +0900

    Feat: 즐겨찾기 등록 및 해제 api 생성
    Former-commit-id: 94390fdb0750b3e54477ad1e78de18731aa96789

---
+++ backend/src/main/java/com/moi/anitime/model/repo/BookmarkRepo.java
@@ -0,0 +1,18 @@
+package com.moi.anitime.model.repo;
+
+import com.moi.anitime.model.entity.bookmark.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookmarkRepo extends JpaRepository<Bookmark, Integer> {
+
+    @Query(value = "SELECT b from Bookmark b WHERE b.animal.desertionNo = :desertionNo AND b.generalMember.memberNo = :generalNo")
+    public Optional<Bookmark> findBookmark(@Param("desertionNo") int desertionNo, @Param("generalNo") int generalNo);
+
+
+}
