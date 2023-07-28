c3393638ea54d29cef725bf39687524ccc8cdd13
김정현 <prairial6@naver.com>
2023-07-28 09:32:32 +0900

    Feat: 입양 신청서 폼 만드는 중
    Former-commit-id: b354b59efd804f3ebdcad63441fb05677702f494

--- frontend/src/components/Desertion/DesertionReservation.js
+++ frontend/src/components/Desertion/DesertionReservation.js
@@ -1,21 +1,93 @@
 import React from "react";
 import { styled } from "styled-components";
 import { MainContainer } from "styled/styled";
-import SelectTime from "../SelectTime/SelectTime";
 import DesertionGuide from "./DesertionGuide";
+import ReservationForm from "components/SelectTime/ReservationForm";
+import SelectTime from "../SelectTime/SelectTime";
 
 export default function DesertionReservation() {
-  const agreed = true;
+  var agreed = true;
+  var selected = false;
+  // 배경색 넣기
   return (
-    <PageContainer>
-      {agreed ? <SelectTime /> : <DesertionGuide />}
-    </PageContainer>
+    <MainContainer
+      style={{
+        width: "100vw",
+        backgroundColor: "#F7F8FA !important",
+      }}
+    >
+      {agreed ? (
+        <div
+          style={{
+            width: "inherit",
+          }}
+        >
+          <ProgressBar>
+            <div>
+              {selected ? (
+                <img src={`/icons/Component 25.svg`} />
+              ) : (
+                <img src={`/icons/Component 24.svg`} />
+              )}
+            </div>
+            <div
+              style={{
+                width: "312px",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={
+                  selected
+                    ? {
+                        color: "#7D848A",
+                      }
+                    : {
+                        color: "#35383B",
+                        fontWeight: "bold",
+                      }
+                }
+              >
+                미팅일자 선택
+              </div>
+              <div
+                style={
+                  selected
+                    ? {
+                        color: "#35383B",
+                        fontWeight: "bold",
+                      }
+                    : {
+                        color: "#7D848A",
+                      }
+                }
+              >
+                신청서 작성
+              </div>
+            </div>
+          </ProgressBar>
+          {selected ? <ReservationForm /> : <SelectTime />}
+        </div>
+      ) : (
+        <DesertionGuide />
+      )}
+    </MainContainer>
   );
 }
 
 const PageContainer = styled.div`
+  border: 2px solid blue;
   margin: auto;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+const ProgressBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 39px;
+  margin-bottom: 74px;
 `;
--- frontend/src/components/SelectTime/SelectTime.js
+++ frontend/src/components/SelectTime/SelectTime.js
@@ -1,70 +1,222 @@
 import { styled } from "styled-components";
 import { useState } from "react";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css'
+import "react-calendar/dist/Calendar.css";
+import dayjs from "dayjs";
 
-export default function SelectTime(){
-    const [date,setDate]=useState(new Date())
-    const [time,setTime]=useState("")
-    const times = Array(10)
+export default function SelectTime() {
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState("");
+  const [impossible, setImpossible] = useState([]);
+  function getImpossible() {
+    // 이미 예약된 시간의 배열 가져옴
+  }
+  const times = Array(10)
     .fill()
     .map((_, index) => {
       return 9 + index + ":00";
     }); // "9:00" ~ "18:00"이 담긴 배열 생성
-    const shelter={
-        name:"싸피 보호소",
-        phone:"010-0000-0000",
-        addr:"서울시 강남구 테헤란로 123-45"
-    }
-    return(<>
-        {<div>{date.getDate()}</div>}
-        <ShelterContainer>
-            <div>{shelter.name}</div>
-            <div>{shelter.phone}</div>
-            <div>{shelter.addr}</div>
-        </ShelterContainer>
-        <MainContainer>
-            <Calendar
-                value={date}
-                onChange={setDate}
-                style={{
-
-                }}
-            ></Calendar>
-            <TimePicker>
-                {time}
-                { times.map((v) => {
-        return <button
-        style={v==time?{
-            color:"blue",
-            backgroundColor:"skyblue",
-        }:{color:"grey"}}
-        onClick={()=>setTime(v)}
-    >
-        {v}
-        </button>})}
-            </TimePicker>
-        </MainContainer>
-    </>);
+  const shelter = {
+    name: "싸피 보호소",
+    phone: "010-0000-0000",
+    addr: "서울시 강남구 테헤란로 123-45",
+  };
+  return (
+    <PageContainer>
+      <ShelterContainer>
+        <div>{shelter.name}</div>
+        <div>
+          <img src={`/icons/ic_call.svg`} alt="연락처" />
+          {shelter.phone}
+        </div>
+        <div>
+          <img src={`/icons/ic_location.svg`} alt="위치" />
+          {shelter.addr}
+        </div>
+      </ShelterContainer>
+      <PickerContainer>
+        <Calendar
+          value={date}
+          onChange={setDate}
+          formatDay={(locale, date) => dayjs(date).format("D")}
+        ></Calendar>
+        <TimePicker>
+          <div
+            style={{
+              fontSize: "16px",
+              display: "flex",
+              width: "280px",
+            }}
+          >
+            미팅 기간
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#7D848A",
+              marginTop: "5px",
+              marginBottom: "5px",
+              fontSize: "14px",
+              borderRadius: "12px",
+              width: "280px",
+              height: "50px",
+              backgroundColor: "#E8EBEE",
+            }}
+          >
+            1시간
+          </div>
+          <div
+            style={{
+              marginTop: "8px",
+              fontSize: "16px",
+              display: "flex",
+              width: "280px",
+            }}
+          >
+            미팅 시간
+          </div>
+          {times.map((v) => {
+            return (
+              <button
+                style={
+                  v === time
+                    ? {
+                        border: "0.77px solid #3994F0",
+                        color: "#3994F0",
+                        backgroundColor: "#E1F0FF",
+                      }
+                    : { color: "#CACED3" }
+                }
+                onClick={() => setTime(v)}
+              >
+                {v}
+              </button>
+            );
+          })}
+          <div
+            style={{
+              fontSize: "12px",
+              color: "#7D848A",
+            }}
+          >
+            미팅 일자: {date.getMonth() + 1}월 {date.getDate()}일 {time}
+          </div>
+          <button id="nextButton">다음</button>
+        </TimePicker>
+      </PickerContainer>
+    </PageContainer>
+  );
 }
-const ShelterContainer=styled.div`
 
-`
-const MainContainer=styled.div`
-    .react-calendar{
-        border:1px solid red;
+const PageContainer = styled.div``;
+const ShelterContainer = styled.div`
+  margin-bottom: 50px;
+  div {
+    &:first-child {
+      font-weight: bold;
+      color: #35383b;
+      font-size: 24px;
     }
-`
+    &:nth-child(2) {
+      color: #535a61;
+      font-size: 14px;
+    }
+    &:last-child {
+      color: #535a61;
+      font-size: 16px;
+    }
+  }
+`;
+const PickerContainer = styled.div`
+  width: 880px;
+  height: 660px;
+  display: flex;
+  //   네비게이션바
 
-const TimePicker=styled.div`
-display:flex;
-flex-direction:column;
-    button{
-        font-size:14px;
-        border-radius:12px;
-        width:280px;
-        height:50px;
-        border:1px solid blue;
-        background-color:transparent;
+  .react-calendar {
+    padding-top: 94px;
+    color: #ffffff;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+    width: 530px;
+    background-color: #535a61;
+    .react-calendar__navigation button {
+      color: #ffffff;
+      min-width: 44px;
+      background: none;
+      font-size: 16px;
+      margin-top: 8px;
     }
-`
+    .react-calendar__navigation button:enabled:hover,
+    .react-calendar__navigation button:enabled:focus {
+      background-color: transparent;
+    }
+    .react-calendar__navigation__label > span {
+      font-size: 24px;
+      font-weight: bold;
+      color: #ffffff;
+    }
+    //타일
+
+    .react-calendar__tile {
+      //   width: 51px;
+      //   height: 51px;
+      //   margin: 10px;
+      width: 71px;
+      height: 71px;
+      color: #ffffff;
+      font-size: 16px;
+      background: transparent;
+      text-align: center;
+    }
+    .react-calendar__tile:enabled:hover,
+    .react-calendar__tile:enabled:focus,
+    .react-calendar__tile--active {
+      background-color: #ffffff;
+      border-radius: 40px;
+      color: #535a61;
+    }
+    .react-calendar__tile--now {
+      background: transparent;
+      color: #ffffff;
+    }
+    .react-calendar__tile--now:enabled:hover,
+    .react-calendar__tile--now:enabled:focus {
+      color: #ffffff;
+    }
+  }
+`;
+
+const TimePicker = styled.div`
+  padding-top: 32px;
+  padding-bottom: 32px;
+  border: 1px solid #e8ebee;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  display: flex;
+  width: 350px;
+  align-items: center;
+  flex-direction: column;
+  button {
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 14px;
+    border-radius: 12px;
+    width: 280px;
+    height: 50px;
+    border: 0.77px solid #e8ebee;
+    background-color: transparent;
+  }
+  #nextButton {
+    color: #ffffff;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 14px;
+    border-radius: 12px;
+    width: 280px;
+    height: 50px;
+    background-color: #3994f0;
+  }
+`;
