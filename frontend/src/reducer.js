6c88755e27578c51cd439dd8e422b6872d47429d
김민태 <kimmintae981019@gmail.com>
2023-08-01 15:58:41 +0900

    Fix: Redux 상태 값 로컬 스토리지에 저장 (로그아웃 시 초기화 필요)
    Former-commit-id: 191ced6d1f45cfe35cf5c8a9e215b6a6966a4f7b

--- frontend/src/reducer.js
+++ frontend/src/reducer.js
@@ -1,11 +1,20 @@
 import { combineReducers } from "redux";
 import memberReducer from "./reducer/member";
 import detailInfoReducer from "./reducer/detailInfo";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["member", "detailInfo"],
+};
 
 const rootReducer = combineReducers({
   member: memberReducer,
   detailInfo: detailInfoReducer,
   // 필요한거 더 추가하세요
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default rootReducer;
+export default persistedReducer;
