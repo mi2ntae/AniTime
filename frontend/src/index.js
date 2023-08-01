2d3956a137f8d2e8fe5f080cdd3bdee19b107fa2
임성원 <dnjsndnjs@gmail.com>
2023-08-01 13:57:36 +0900

    Feat: ErrorPage 설정
    Former-commit-id: 764aab32740b1869d558bba185a9c10076bfb02c

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 
 import router from "./router.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
