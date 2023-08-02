9ec735bf80edbb940b534d0312113fca4c854b11
김정현 <prairial6@naver.com>
2023-08-02 17:12:48 +0900

    Feat: 입양 동물 목록 조회
    Former-commit-id: 98d85601ffefd59f48910667efeab814c67bbf76
    Former-commit-id: 8bc6e0f61da508fd25803c34f3ea479646d4786a

--- backend/src/main/java/com/moi/anitime/model/repo/AnimalRepo.java
+++ backend/src/main/java/com/moi/anitime/model/repo/AnimalRepo.java
@@ -1,5 +1,6 @@
 package com.moi.anitime.model.repo;
 
+import com.moi.anitime.api.response.animal.AnimalPreviewRes;
 import com.moi.anitime.model.entity.animal.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,10 +13,8 @@ import java.util.Optional;
 @Repository
 public interface AnimalRepo extends JpaRepository<Animal, Integer> {
 
-    @Query(value = "SELECT A.*,IF(B.bookmarkNo IS NOT NULL,'Y','N') AS isBookMarked FROM Animal AS A " +
-            "LEFT JOIN Bookmark AS B ON A.desertionNo=B.desertionNo AND B.generalNo=:generalNo"+
-            "WHERE kind LIKE :kind AND sexcd LIKE :sexcd ORDER BY :sortQuery", nativeQuery = true)
-    public List<Animal> getAnimal(int generalNo,String kind, char sexcd, String sortQuery, Pageable curPageNo);
+    @Query(name = "getAnimal", nativeQuery = true)
+    public List<AnimalPreviewRes> getAnimal(int generalNo, String kind, char sexcd, String sortQuery, Pageable curPageNo);
 
     //    @Query("SELECT a.desertionNo, a.kind, a.sexcd, a.image1 FROM Animal a JOIN Bookmark b WHERE b.generalMember.memberNo = :generalNo ORDER BY b.bookmarkNo DESC")
     @Query(value = "SELECT a FROM Animal a JOIN Bookmark b ON a.desertionNo = b.animal.desertionNo WHERE b.generalMember.memberNo = :generalNo ORDER BY b.bookmarkNo DESC")
