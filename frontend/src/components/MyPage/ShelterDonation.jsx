7463b73b0a5f1d85731f23abcc36db5b216fa0ff
임성원 <dnjsndnjs@gmail.com>
2023-08-02 12:09:15 +0900

    Feat: 보호소 후원 내역탭 컴포넌트
    Former-commit-id: e7c4720715666729856325635ed4ffac7025f5e6
    Former-commit-id: 5e9c6d74c324d28a7ca8f8b57d60300e886db9bf

--- frontend/src/components/MyPage/ShelterDonation.jsx
+++ frontend/src/components/MyPage/ShelterDonation.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { styled } from "styled-components";
+import { Button } from "styled/styled";
 
 export default function ShelterDonation() {
-  return <p>후원 현황</p>;
+  return (
+    <>
+      <TabHeader>
+        <TabTitle>후원 현황</TabTitle>
+        <Button $background_color="#3994F0" color="#FFFFFF">
+          후원공고 등록하기
+        </Button>
+      </TabHeader>
+      <div>내용?</div>
+    </>
+  );
 }
+
+const TabHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 24px 0;
+`;
+const TabTitle = styled.span`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 16px;
+`;
