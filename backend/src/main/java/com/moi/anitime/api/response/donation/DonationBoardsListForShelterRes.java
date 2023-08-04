9af66a859b3f35f9371cf646ecf9cd1fa7dfe13a
lojo0307 <lojo0307@naver.com>
2023-08-04 15:59:27 +0900

    Feat: 후원공고 등록 및 조회 API 응답 객체 및 검색조건 수정
    Former-commit-id: e0e6b749c5fb7619e9e117ac956475e3beed9066

---
+++ backend/src/main/java/com/moi/anitime/api/response/donation/DonationBoardsListForShelterRes.java
@@ -0,0 +1,15 @@
+package com.moi.anitime.api.response.donation;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class DonationBoardsListForShelterRes {
+
+    private int status;
+    private String title;
+    private String date;
+    private String attained;
+    private String goal;
+}
