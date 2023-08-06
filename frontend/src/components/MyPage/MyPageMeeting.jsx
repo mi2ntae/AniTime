c9f7050737b9715263bd51318ce3d4b02f65500e
임성원 <dnjsndnjs@gmail.com>
2023-08-06 18:18:30 +0900

    Feat: 일반회원 마이페이지 미팅내역 api 연결
    Former-commit-id: 95791e7b6c3f0a43dfe86d8c608b07bcb251b7a3

--- frontend/src/components/MyPage/MyPageMeeting.jsx
+++ frontend/src/components/MyPage/MyPageMeeting.jsx
@@ -7,39 +7,63 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
+import http from "api/commonHttp";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { styled } from "styled-components";
 import { Button } from "styled/styled";
 
 export default function MyPageMeeting() {
+  const member = useSelector((state) => state.member);
+
   const [meetings, setMeetings] = useState([]);
   const [pageNo, setPageNo] = useState(1);
-  const [maxPage, setMaxPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(1);
+
+  const processState = (item) => {
+    let res = undefined;
+    switch (item.state) {
+      case 0:
+        res = <StateText color="#535A61">승인대기</StateText>;
+        break;
+      case 1:
+        const rDate = new Date(item.reservedDate);
+        const nDate = new Date();
+        const diff = rDate.getTime() - nDate.getTime();
+        // 30분
+        if (diff < -1800000) {
+          res = <StateText color="#535A61">종료</StateText>;
+        } else if (diff < 1800000) {
+          res = (
+            <Button
+              $background_color="#3994F0"
+              color="#FFFFFF"
+              style={{ fontWeight: "bold" }}
+              onClick={() => console.log(item.meetNo)}
+            >
+              미팅 참여
+            </Button>
+          );
+        } else res = <StateText color="#3994F0">승인</StateText>;
+        break;
+      case 2:
+        res = <StateText color="#FF7676">반려</StateText>;
+    }
+    return res;
+  };
 
   useEffect(() => {
     // api 통신
-    const testData = [
-      {
-        meetNo: 12345,
-        member: {
-          name: "김민태",
-        },
-        reservedDate: "2023-07-24 15:00",
-        status: "종료",
-      },
-      {
-        meetNo: 12347,
-        member: {
-          name: "김민태",
-        },
-        reservedDate: "2023-07-24 15:00",
-        status: "종료",
-      },
-    ];
-    console.log("pageNo: " + pageNo);
-    setMeetings(testData);
-    setMaxPage(5);
-  }, [pageNo]);
+    http
+      .get(`meet/${member.memberNo}/?page=${pageNo - 1}`)
+      .then(({ data: { content, totalPages } }) => {
+        setMeetings(content);
+        setMaxPage(totalPages);
+      })
+      .catch(() => {
+        console.log("미팅 목록 조회 실패");
+      });
+  }, [pageNo, member.memberNo]);
 
   return (
     <MainDiv>
@@ -57,15 +81,11 @@ export default function MyPageMeeting() {
           <TableBody>
             {meetings.map((item) => (
               <TableRow key={item.meetNo}>
-                <TableCell>{item.member.name}</TableCell>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.meetContent}</TableCell>
+                <TableCell>{item.desertionNo}</TableCell>
                 <TableCell>{item.reservedDate}</TableCell>
-                <TableCell>{item.reservedDate}</TableCell>
-                <TableCell>{item.reservedDate}</TableCell>
-                <TableCell>
-                  <Button onClick={() => console.log(item.meetNo)}>
-                    {item.status}
-                  </Button>
-                </TableCell>
+                <TableCell>{processState(item)}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -86,4 +106,9 @@ const MainDiv = styled.div`
   align-items: center;
   justify-content: space-between;
   height: 100%;
+`;
+
+const StateText = styled.span`
+  font-weight: bold;
+  color: ${({ color }) => color || "black"};
 `;
