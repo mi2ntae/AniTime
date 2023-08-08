0a5282bc012c2e0b4127525f63d6c56630bec43b
임성원 <dnjsndnjs@gmail.com>
2023-08-08 14:49:27 +0900

    Feat: 메인페이지 슬라이더 인터벌 수정
    Former-commit-id: 9fc76bfb49336c0e42243a53082170efe4131fa8

--- frontend/src/components/Main/Slider.js
+++ frontend/src/components/Main/Slider.js
@@ -9,40 +9,59 @@ export default function Slider() {
   const sliderRef = useRef();
   const index = useRef(0);
   const animalLen = useRef(1);
+  const interval = useRef();
+  const intervalTime = 3000;
 
-  const moveIndex = useCallback(
+  /**
+   * index를 변경하여 슬라이더를 이동하는 함수
+   */
+  const moveIndex = useCallback((offset) => {
+    const prev = index.current;
+    index.current += offset;
+    const sliderWidth = sliderRef.current.offsetWidth / 3;
+    const itemWidth = sliderWidth / animalLen.current;
+    sliderRef.current.animate(
+      {
+        transform: [
+          `translateX(${-sliderWidth - prev * itemWidth}px)`,
+          `translateX(${-sliderWidth - index.current * itemWidth}px)`,
+        ],
+      },
+      {
+        duration: 500, // 밀리초 지정
+        fill: "forwards", // 종료 시 속성을 지님
+        easing: "ease", // 가속도 종류
+      }
+    );
+    index.current = (index.current + animalLen.current) % animalLen.current;
+  }, []);
+
+  /**
+   * 좌우 버튼 클릭시 작동하는 함수
+   * 설정된 interval을 해제하고 moveIndex 실행후 다시 새로운 interval 설정
+   * 좌우 버튼 클릭시에도 interval이 남아 시간을 세는 것을 방지하기 위한 함수
+   */
+  const handleClick = useCallback(
     (offset) => {
-      const prev = index.current;
-      index.current += offset;
-      const sliderWidth = sliderRef.current.offsetWidth / 3;
-      const itemWidth = sliderWidth / animalLen.current;
-      sliderRef.current.animate(
-        {
-          transform: [
-            `translateX(${-sliderWidth - prev * itemWidth}px)`,
-            `translateX(${-sliderWidth - index.current * itemWidth}px)`,
-          ],
-        },
-        {
-          duration: 500, // 밀리초 지정
-          fill: "forwards", // 종료 시 속성을 지님
-          easing: "ease", // 가속도 종류
-        }
-      );
-      index.current = (index.current + animalLen.current) % animalLen.current;
-      console.log(index.current);
+      clearInterval(interval.current);
+      moveIndex(offset);
+      interval.current = setInterval(() => {
+        moveIndex(1);
+      }, intervalTime);
     },
-    [animals]
+    [moveIndex]
   );
 
+  /**
+   * api를 사용하여 백엔드에서 데이터 가져오기
+   * 무한 슬라이더를 위해 앞뒤로 붙임
+   */
   useEffect(() => {
-    // Todo: 서버와 통신하여 데이터 받기
     http
       .get(
         `desertion?generalNo=0&kindType=0&genderType=0&sortType=0&curPageNo=0`
       )
       .then(({ data }) => {
-        console.log(data);
         setAnimals([...data, ...data, ...data]);
         animalLen.current = data.length;
       })
@@ -51,14 +70,17 @@ export default function Slider() {
       });
   }, []);
 
+  /**
+   * 최초에 슬라이더에 interval을 설정해주는 함수
+   * unmount시에 interval 해제
+   */
   useEffect(() => {
-    // 자동 슬라이더
-    // const interval = setInterval(() => {
-    //   moveIndex(1);
-    // }, 3000);
-    // return () => {
-    //   clearInterval(interval);
-    // };
+    interval.current = setInterval(() => {
+      moveIndex(1);
+    }, intervalTime);
+    return () => {
+      clearInterval(interval.current);
+    };
   }, [moveIndex]);
 
   return (
@@ -69,10 +91,10 @@ export default function Slider() {
         ))}
       </SliderDiv>
       <ButtonDiv>
-        <Button onClick={() => moveIndex(-1)}>
+        <Button onClick={() => handleClick(-1)}>
           <img src="icons/ic_arrow_left.svg" alt="left" />
         </Button>
-        <Button onClick={() => moveIndex(1)}>
+        <Button onClick={() => handleClick(1)}>
           <img src="icons/ic_arrow_right.svg" alt="right" />
         </Button>
       </ButtonDiv>
