b5f000b27df917a8e4fa612ae39eb61eab3a024c
임성원 <dnjsndnjs@gmail.com>
2023-08-09 17:10:24 +0900

    Feat: 알림창 api 조회
    Former-commit-id: 978d62d4b4186299d43cea13bfdb3253eb1c777a

--- frontend/src/components/Notice/NoticeContainer.js
+++ frontend/src/components/Notice/NoticeContainer.js
@@ -1,70 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { styled } from "styled-components";
 import NoticeItem from "./NoticeItem";
+import http from "api/commonHttp";
+import { useSelector } from "react-redux";
 
 export default function NoticeContainer() {
-  const [selected, setSelected] = useState(4);
   const [noticeList, setNoticeList] = useState([]);
+  const member = useSelector((state) => state.member);
+
+  const getNotice = useCallback(() => {
+    http
+      .get(`notice?memberNo=${member.memberNo}`)
+      .then(({ data }) => {
+        setNoticeList(data);
+        data.forEach((element) => {
+          http
+            .put(`notice/${element.noticeNo}`)
+            .catch((error) => console.error(error));
+        });
+      })
+      .catch((error) => console.error(error));
+  }, [member]);
+
+  const deleteNotice = useCallback(() => {
+    setNoticeList([]);
+    http
+      .delete(`notice/${member.memberNo}`)
+      .catch((error) => console.error(error));
+  }, [member]);
 
   useEffect(() => {
-    // Todo: 서버와 통신하여 실제 알림 받아오는 코드 적용
-    const testData = [
-      {
-        noticeNo: 12,
-        noticeKind: 0,
-        noticeTime: "0000.00.00 00:00:00",
-        noticeContent:
-          "민태님의 싸피 보호소 미팅 예약 (2023년 00월 00일 00시 00분) 신청이 승인되었습니다.",
-      },
-      {
-        noticeNo: 13,
-        noticeKind: 1,
-        noticeTime: "0000.00.00 00:00:00",
-        noticeContent: "금일 알람이지롱1",
-      },
-      {
-        noticeNo: 14,
-        noticeKind: 3,
-        noticeTime: "0000.00.00 00:00:00",
-        noticeContent: "금일 알람이지롱2",
-      },
-    ];
-    setNoticeList(testData);
-  }, []);
+    getNotice();
+  }, [getNotice]);
 
   return (
     <Container>
-      <Header>
-        <HeaderItem $selected={selected === 4} onClick={() => setSelected(4)}>
-          전체
-        </HeaderItem>
-        <HeaderItem $selected={selected === 0} onClick={() => setSelected(0)}>
-          입양
-        </HeaderItem>
-        <HeaderItem $selected={selected === 1} onClick={() => setSelected(1)}>
-          실종
-        </HeaderItem>
-        <HeaderItem $selected={selected === 2} onClick={() => setSelected(2)}>
-          후원
-        </HeaderItem>
-        <HeaderItem $selected={selected === 3} onClick={() => setSelected(3)}>
-          채팅
-        </HeaderItem>
-      </Header>
       <Body>
         {noticeList.length === 0
           ? "알림이 없습니다"
-          : noticeList
-              .filter((item) => selected === 4 || item.noticeKind === selected)
-              .map((item) => <NoticeItem {...item} key={item.noticeNo} />)}
+          : noticeList.map((item) => (
+              <NoticeItem {...item} key={item.noticeNo} />
+            ))}
       </Body>
-      <Footer
-        onClick={() => {
-          setNoticeList([]);
-        }}
-      >
-        알림함 비우기
-      </Footer>
+      <Footer onClick={deleteNotice}>알림함 비우기</Footer>
     </Container>
   );
 }
@@ -74,35 +52,16 @@ const Container = styled.div`
   height: 360px;
 `;
 
-const Header = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  height: 50px;
-  box-sizing: border-box;
-  border-bottom: grey 1px solid;
-`;
-
-const HeaderItem = styled.p`
-  display: flex;
-  align-items: center;
-  margin: 0;
-  margin-left: 24px;
-  height: 100%;
-  box-sizing: border-box;
-  border-bottom: ${(props) => (props.$selected ? "black" : "white")} 2px solid;
-  cursor: pointer;
-`;
-
 const Body = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
   overflow: auto;
-  height: calc(100% - 100px);
+  height: calc(100% - 50px);
   box-sizing: border-box;
-  padding-top: 16px;
+  padding: 16px;
+  text-align: center;
+  row-gap: 16px;
 `;
 
 const Footer = styled.div`
