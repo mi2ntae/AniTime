461b840d38606668161b858867af4b227aee3542
권지훈 <aspire51@naver.com>
2023-08-11 15:35:41 +0900

    feat : desertionMap frontend
    Former-commit-id: a0f726ae9e0402efb003cf3387fe2c59a5b88bf9
    Former-commit-id: f910f10855426b8cfdbc0c2d1bc43aa47eb0af9d

---
+++ frontend/src/components/Main/DesertionMap.jsx
@@ -0,0 +1,47 @@
+import { SimpleSouthKoreaMapChart } from "components/SimpleSouthKoreaMapChart";
+import { useEffect, useState } from "react";
+import http from "api/commonHttp";
+
+export default function LoginPage() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    if (data.length == 0) fetchData();
+  }, [data]);
+  const fetchData = async () => {
+    try {
+      let response = await http.get(`count`);
+      let newdata = await response.data;
+      newdata.reduce((acc, cur) => {
+        console.log(cur);
+        setData((p) => [...p, { locale: cur.mapName, count: cur.entryNumber }]);
+      });
+      console.log(data);
+    } catch (error) {
+      console.log("에러메시지: ", error);
+    }
+  };
+  const setColorByCount = (count) => {
+    if (count === 0) return "#F1F1F1";
+    if (count > 5000) return "#79D3C4";
+    if (count > 3000) return "#43cdb6";
+    if (count > 1000) return "#61CDBB";
+    if (count > 200) return "#91D9CD";
+    if (count > 100) return "#A9DFD6";
+    if (count > 50) return "#C1E5DF";
+    if (count > 5) return "#D9EBE8";
+    else return "#ebfffd";
+  };
+
+  return (
+    <div
+      style={{
+        width: "80%",
+        height: "400px",
+        padding: "20px",
+        background: "white",
+      }}
+    >
+      <SimpleSouthKoreaMapChart setColorByCount={setColorByCount} data={data} />
+    </div>
+  );
+}
