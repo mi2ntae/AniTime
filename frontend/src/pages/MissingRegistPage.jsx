802914cb432b7577343ea881c3adc5eea293d4ea
lojo0307 <lojo0307@naver.com>
2023-08-14 14:03:51 +0900

    Fix: 사진 사이즈는 어쩌고
    Former-commit-id: 2a296fa24e2fc1e4da621e3c04f0a24b66debc1a
    Former-commit-id: 376e130e5a446660a8fc3105360127657bd7b7f2

--- frontend/src/pages/MissingRegistPage.jsx
+++ frontend/src/pages/MissingRegistPage.jsx
@@ -159,6 +159,14 @@ export default function MissingRegistPage() {
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     if (!file) return;
+
+    var maxSize = 5 * 1024 * 1024;
+    var fileSize = file.size;
+
+    if (fileSize > maxSize) {
+      alert("파일 사이즈는 5MB 이내로 등록 가능합니다.");
+      return;
+    }
     if (!file.type.startsWith("image/")) {
       alert("이미지 형식의 파일만 등록 가능합니다.");
       return;
@@ -401,7 +409,7 @@ export default function MissingRegistPage() {
                   marginTop: "10px",
                 }}
               >
-                사진 사이즈는 어쩌고
+                사진은 5MB 이내의 jpg, png, gif 파일만 등록 가능합니다.
               </div>
             </div>
           </div>
