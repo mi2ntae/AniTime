ab52ffda8f3c4e8eff3b01841b9948d6cc8c5d09
lojo0307 <lojo0307@naver.com>
2023-08-14 13:07:01 +0900

    Fix: 자잘자잘 수정
    Former-commit-id: 620193a77ff7d62a57de22e3c8cf24250bb2810c
    Former-commit-id: 2205daf709a194987bd321cd5579416a6419811d

--- frontend/src/pages/MissingRegistPage.jsx
+++ frontend/src/pages/MissingRegistPage.jsx
@@ -15,6 +15,7 @@ import {
   MainContainer,
 } from "styled/styled";
 import SelectBox from "components/Profile/SelectBox";
+import DescModal from "components/Profile/DescModal";
 
 export default function MissingRegistPage() {
   const general = useSelector((state) => state.member);
@@ -425,6 +426,7 @@ export default function MissingRegistPage() {
         </form>
       </WriteContainer>
       {modal && <MapComponent setModal={setModal} getPosition={getPosition} />}
+      <DescModal />
     </MainContainer>
   );
 }
